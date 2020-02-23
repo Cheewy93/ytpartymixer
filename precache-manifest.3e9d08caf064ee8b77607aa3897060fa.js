e573237e31d5c51443e260a489fef037ee624765
v.rakovic <v.rakovic@vegaitsourcing.rs>
2020-02-24 00:33:59 +0100

    Updates

---
+++ precache-manifest.3e9d08caf064ee8b77607aa3897060fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2afa847777eb3b5583e849ae4c3c810",
+    "url": "/ytpartymixer/index.html"
+  },
+  {
+    "revision": "ff9fb02ac836afb3c2d5",
+    "url": "/ytpartymixer/static/css/2.c05e4ea7.chunk.css"
+  },
+  {
+    "revision": "78d72ec1e867903623b0",
+    "url": "/ytpartymixer/static/css/main.19c617e8.chunk.css"
+  },
+  {
+    "revision": "ff9fb02ac836afb3c2d5",
+    "url": "/ytpartymixer/static/js/2.ebfc8452.chunk.js"
+  },
+  {
+    "revision": "78d72ec1e867903623b0",
+    "url": "/ytpartymixer/static/js/main.a734b948.chunk.js"
+  },
+  {
+    "revision": "d98d33ea52042c763337",
+    "url": "/ytpartymixer/static/js/runtime-main.e2814000.js"
+  },
+  {
+    "revision": "70392d3b719f46cda692930bf3b61a43",
+    "url": "/ytpartymixer/static/media/loading.70392d3b.gif"
+  },
+  {
+    "revision": "ba6b12ca852d7c9f0a317e9fefd02a89",
+    "url": "/ytpartymixer/static/media/logo.ba6b12ca.png"
+  }
+]);
